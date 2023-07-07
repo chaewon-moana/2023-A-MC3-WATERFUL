0a24d3d86d912db14f2b85500257f8fc2fe05e4b
soc06212 <soc06212@gmail.com>
2023-07-07 15:40:36 +0900

    Initial Commit

---
+++ GitSetKey/GitSetKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSetKeyApp.swift
+//  GitSetKey
+//
+//  Created by 최명근 on 2023/07/07.
+//
+
+import SwiftUI
+
+@main
+struct GitSetKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
