06e6afdb03778c288ed81e5ddf019bc46174842c
songmoro <97587585+songmoro@users.noreply.github.com>
2023-07-15 00:24:06 +0900

    feat: Core Data 사용을 위한 작업
    1. PersistenceController에 Team 데이터 작업을 위한 CRUD를 만들었습니다.
    2. Team 데이터의 작업을 할 수 있도록 View Model 및 함수들을 추가 했습니다.
    3. ContentView에 Team View Model을 추가하고 Team View Model의 동작 테스트를 위한 코드를 작성 했습니다.
    4. PersistenceController의 share의 전역 변수화로 Environment를 사용할 필요가 없게되어 GitSetKitApp의 해당 코드를 삭제했습니다.
    5. 주석의 통일을 위해 // MARK: 의 형식을 // MARK: - <주체> 형식으로 바꾸고 구분이 필요해 보이는 코드의 후미에 //: - <주체>를 추가 했습니다.

--- GitSetKit/Window/ContentView.swift
+++ GitSetKit/Window/ContentView.swift
@@ -8,32 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @State private var columnVisibility: NavigationSplitViewVisibility = .doubleColumn
     
-    @State private var teams: [Team] = []
-    @State private var selected: Team?
+    @StateObject var teamVM: TeamViewModel = TeamViewModel()
     
     var body: some View {
         NavigationSplitView(columnVisibility: $columnVisibility) {
-            // MARK: Side Bar
-            TeamView(teams: $teams, selected: $selected)
-            
-        } detail: {
-            // MARK: Detail
-            if let selected = selected {
-                ConventionView(selected: $selected)
-            } else {
-                VStack(spacing: 16) {
-                    Image(systemName: "tray")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 64, height: 64)
-                    Text("not_selected")
+            // MARK: - Side Bar
+            //            TeamView(teams: $teams, selected: $selected)
+            ForEach(teamVM.teams) { team in
+                HStack {
+                    Text("\(team.name ?? "Unknown Team")")
+                    Text("\(team.desc ?? "Unknown Desc")")
+                }
+                .onTapGesture {
+                    teamVM.deleteTeam(team: team)
                 }
             }
+            
+            Button("Add Test") {
+                teamVM.name = "name"
+                teamVM.desc = "desc"
+                teamVM.template = "teamplate"
+                
+                teamVM.createTeam()
+                teamVM.clearStates()
+            }
+            //: - Side Bar
+        } detail: {
+            // MARK: - Detail
+            //            if let selected = selected {
+            //                ConventionView(selected: $selected)
+            //            } else {
+            //                VStack(spacing: 16) {
+            //                    Image(systemName: "tray")
+            //                        .resizable()
+            //                        .aspectRatio(contentMode: .fit)
+            //                        .frame(width: 64, height: 64)
+            //                    Text("not_selected")
+            //: - Detail
         }
-
     }
 }
 
