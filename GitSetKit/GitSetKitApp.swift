36904a2a35eb2b6fb8e2bf62c8e6086129659ed2
soc06212 <soc06212@gmail.com>
2023-07-20 23:26:25 +0900

    feat: PersistenceController 의존성 주입

--- GitSetKit/GitSetKitApp.swift
+++ GitSetKit/GitSetKitApp.swift
@@ -10,9 +10,12 @@ import SwiftUI
 @main
 struct GitSetKitApp: App {
     
+    let persistenceController = PersistenceController.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
         .commands {
             
