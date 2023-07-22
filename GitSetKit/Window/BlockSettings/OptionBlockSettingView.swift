e040980232de8342d07e76000d0d75a409842381
songmoro <97587585+songmoro@users.noreply.github.com>
2023-07-23 01:43:04 +0900

    feat: OptionBlockSettingView 수정 1. OptionBlockSettingView의 inputFIeld를 함수화하여 호출하는 방식으로 변경하였습니다. 2. localization의 option_block_field_detail을 "Detail Description"로 수정하였습니다

--- GitSetKit/Window/BlockSettings/OptionBlockSettingView.swift
+++ GitSetKit/Window/BlockSettings/OptionBlockSettingView.swift
@@ -11,43 +11,63 @@ struct OptionBlockSettingView: View {
     var body: some View {
         ScrollView(.vertical) {
             // MARK: d
-            inputField
+            ForEach(0..<optionCount, id: \.self) { idx in
+                inputField(idx: idx)
+            }
         }
+        .shadow(radius: 1)
         .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 4)
+                .fill(Colors.Gray.quaternary)
+        )
     }
+    @State private var optionCount = 1
     
     @State private var value: String = ""
     @State private var short: String = ""
     @State private var detail: String = ""
-    var inputField: some View {
-        HStack {
-            TextField("", text: $value, prompt: Text("option_block_field_value"))
-                .textFieldStyle(.plain)
-                .multilineTextAlignment(.center)
-            Divider()
-            TextField("", text: $value, prompt: Text("option_block_field_short"))
-                .textFieldStyle(.plain)
-                .multilineTextAlignment(.center)
-            Divider()
-            TextField("", text: $value, prompt: Text("option_block_field_detail"))
-                .textFieldStyle(.plain)
-                .multilineTextAlignment(.center)
-            Button {
-                
-            } label: {
-                Image(systemName: "plus")
+    
+    func inputField(idx: Int) -> some View {
+        return HStack {
+            Group {
+                TextField("", text: $value, prompt: Text("option_block_field_value"))
+                    .textFieldStyle(.plain)
+                    .multilineTextAlignment(.center)
+                Divider()
+                TextField("", text: $short, prompt: Text("option_block_field_short"))
+                    .textFieldStyle(.plain)
+                    .multilineTextAlignment(.center)
+                Divider()
+                TextField("", text: $detail, prompt: Text("option_block_field_detail"))
+                    .textFieldStyle(.plain)
+                    .multilineTextAlignment(.center)
             }
-            .buttonStyle(.plain)
-            .frame(width: 24, height: 24)
-            .background(
-                RoundedRectangle(cornerRadius: 8)
-                    .fill(Color.accentColor)
-            )
+            
+            if idx == 0 {
+                Button {
+                    optionCount += 1
+                } label: {
+                    Image(systemName: "plus")
+                        .foregroundColor(Colors.Text.primary)
+                }
+                .buttonStyle(.plain)
+                .frame(width: 24, height: 24)
+                .background(
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color.accentColor)
+                )
+            }
+            else {
+                Rectangle()
+                    .fill(.clear)
+                    .frame(width: 24, height: 24)
+            }
         }
         .padding(4)
         .background(
             RoundedRectangle(cornerRadius: 8)
-                .fill(Color.gray)
+                .fill(Color.white)
         )
     }
 }
